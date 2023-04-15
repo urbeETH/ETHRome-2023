31efbf196ebf040e422aca2645488f38ed0bf6f3
PaoloRollo <paolo.rollo1997@gmail.com>
2023-04-15 15:06:10 +0200

    fix: intro spacing and typos.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,41 +4,51 @@ import Header from './Header'
 export default function Hero() {
   return (
     <div className='h-screen relative bg-[#EFEBE6] overflow-hidden'>
-      <div className='flex flex-col items-center justify-center h-full'>
+      <div className='flex flex-col items-center justify-start md:justify-center pt-28 md:pt-0 h-full'>
         <div className='flex flex-col md:flex-row items-center space-y-8 md:space-y-0 space-x-0 md:space-x-8 z-10 w-full md:w-auto'>
           <div className='relative'>
             <img src='splash_logo.png' className='hidden md:block' />
             <img
               src='ethrome_logo.svg'
-              className='relative md:absolute md:bottom-20 md:right-4 h-48 md:h-auto'
+              className='relative md:absolute md:bottom-20 md:right-4 h-56 md:h-auto'
             />
           </div>
-          <div className='flex flex-col items-center md:items-start space-y-8 md:space-y-2 w-full md:w-auto px-4'>
-            <h1 className='text-5xl md:text-7xl'>
-              <span className='font-bold'>ETH</span> <span className='font-light'>Rome</span>
-            </h1>
-            <h2 className='text-2xl md:text-4xl font-normal text-center md:text-left max-w-[240px] md:max-w-none'>
-              Hacking history, shaping the future
-            </h2>
-            <h3 className='hidden md:block font-semibold mt-8 text-xl'>6-8th October 2023</h3>
-            <p className='hidden md:block text-xl'>Governance & Privacy Hackathon</p>
-            <div className='flex flex-col md:flex-row items-center space-x-0 md:space-x-4 mt-4 w-full space-y-4 md:space-y-0'>
+          <div className='flex flex-col items-center md:items-start space-y-8 md:space-y-8 w-full md:w-auto px-4'>
+            <div className='flex flex-col items-center md:items-start'>
+              <h1 className='text-5xl md:text-7xl'>
+                <span className='font-bold'>ETH</span>
+                <span className='font-light'>Rome</span>
+              </h1>
+              <h2 className='text-2xl md:text-4xl font-normal text-center md:text-left max-w-[240px] md:max-w-none mt-2 md:mt-0'>
+                Hacking history, shaping the future
+              </h2>
+            </div>
+            <div className='hidden md:flex flex-col'>
+              <h3 className='font-semibold text-xl'>6-8th October 2023</h3>
+              <p className='text-xl'>Governance & Privacy Hackathon</p>
+            </div>
+            <div className='flex flex-col md:flex-row items-center space-x-0 md:space-x-4 mt-4 w-5/6 space-y-4 md:space-y-0'>
+              <div
+                onClick={() => window.open('/ETHROME_SPONSOR_DECK.pdf', '_blank')}
+                className='w-full md:w-40 bg-[#8314C7] hover:bg-[#5b0e8b] transition-colors drop-shadow-[0_10px_8px_rgba(131,20,199,0.25)] rounded-full flex items-center justify-center text-white py-2 font-medium cursor-pointer'
+              >
+                <p>Sponsor deck</p>
+              </div>
+
               <div
                 onClick={() => window.open('https://twitter.com/ethereumrome', '_blank')}
-                className='w-full md:w-40 bg-[#8314C7] hover:bg-[#5b0e8b] transition-colors drop-shadow-[0_10px_8px_rgba(131,20,199,0.25)] rounded-full flex items-center justify-center text-white py-2 font-medium cursor-pointer'
+                className='w-full md:w-40 bg-transparent border-2 border-[#8314C7] hover:border-[#5b0e8b] hover:text-[#5b0e8b] text-[#8314C7] drop-shadow-[0_10px_8px_rgba(131,20,199,0.25)] rounded-full flex items-center justify-center py-2 font-medium cursor-pointer'
               >
                 <Icon icon='mdi:twitter' className='text-2xl cursor-pointer' />
                 <p>Stay tuned</p>
               </div>
-
-              <div
-                onClick={() => window.open('/ETHROME_SPONSOR_DECK.pdf', '_blank')}
-                className='w-full md:w-40 bg-transparent border-2 border-[#8314C7] hover:border-[#5b0e8b] hover:text-[#5b0e8b] text-[#8314C7] drop-shadow-[0_10px_8px_rgba(131,20,199,0.25)] rounded-full flex items-center justify-center py-2 font-medium cursor-pointer'
-              >
-                <p>Sponsor deck</p>
-              </div>
             </div>
           </div>
+        </div>
+
+        <div className='flex md:hidden flex-col text-black z-10 text-center absolute bottom-12'>
+          <h3 className='font-semibold text-xl'>6-8th October 2023</h3>
+          <p className='text-xl'>Governance & Privacy Hackathon</p>
         </div>
         <img
           src='splash_white_mobile.png'
--- src/components/SponsorZone.tsx
+++ src/components/SponsorZone.tsx
@@ -1,10 +1,10 @@
 export default function SponsorZone() {
   return (
     <div
-      className='h-screen md:h-auto bg-[#191818] relative pt-8 md:pt-32 pb-48 xl:py-48 pl-0 md:pl-48'
+      className='min-h-screen md:h-auto bg-[#191818] relative pt-8 md:pt-32 pb-48 xl:py-48 pl-0 md:pl-48'
       id='contact'
     >
-      <div className='flex flex-col justify-center items-center md:items-start max-w-xl space-y-4 relative'>
+      <div className='flex flex-col justify-center items-center md:items-start max-w-xl space-y-4 relative pb-24'>
         <img src='/sponsor_crown.svg' className='h-[160px] w-[160px]' />
         <h1 className='font-bold text-white text-5xl md:text-7xl'>Sponsor Zone</h1>
         <p className='text-white max-w-xs md:max-w-[385px] text-center md:text-left'>
@@ -12,7 +12,7 @@ export default function SponsorZone() {
           demonstrate your unwavering support for the development of the blockchain space!
         </p>
         <div
-          // onClick={() => window.open('/SPONSOR_DECK_ETHROME.pdf', '_blank')}
+          onClick={() => window.open('/SPONSOR_DECK_ETHROME.pdf', '_blank')}
           className='w-40 hover:bg-[#5b0e8b] transition-colors bg-[#8314C7] drop-shadow-[0_10px_8px_rgba(131,20,199,0.25)] rounded-full flex items-center justify-center text-white py-2 font-medium cursor-pointer'
         >
           <p>Sponsor deck</p>
