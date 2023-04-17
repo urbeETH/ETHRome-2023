be987fe88517ecca97a22b47379e553e8084f4b2
colthreepv <valerio.coltre@gmail.com>
2023-04-17 11:32:39 +0200

    fixup sponsor deck link, reusable

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,19 @@
 import { Icon } from '@iconify/react'
-import Header from './Header'
+import { SponsorDeckLink } from './SponsorZone'
 
 export default function Hero() {
   return (
     <div className='h-screen relative bg-[#EFEBE6] overflow-hidden'>
-      <div className='flex flex-col items-center justify-start md:justify-center pt-28 md:pt-0 h-full'>
-        <div className='flex flex-col md:flex-row items-center space-y-8 md:space-y-0 space-x-0 md:space-x-8 z-10 w-full md:w-auto'>
+      <div className='flex flex-col items-center justify-start h-full md:justify-center pt-28 md:pt-0'>
+        <div className='z-10 flex flex-col items-center w-full space-x-0 space-y-8 md:flex-row md:space-y-0 md:space-x-8 md:w-auto'>
           <div className='relative'>
             <img src='splash_logo.png' className='hidden md:block' />
             <img
               src='ethrome_logo.svg'
-              className='relative md:absolute md:bottom-20 md:right-4 h-56 md:h-auto'
+              className='relative h-56 md:absolute md:bottom-20 md:right-4 md:h-auto'
             />
           </div>
-          <div className='flex flex-col items-center md:items-start space-y-8 md:space-y-8 w-full md:w-auto px-4'>
+          <div className='flex flex-col items-center w-full px-4 space-y-8 md:items-start md:space-y-8 md:w-auto'>
             <div className='flex flex-col items-center md:items-start'>
               <h1 className='text-5xl md:text-7xl'>
                 <span className='font-bold'>ETH</span>
@@ -23,17 +23,12 @@ export default function Hero() {
                 Hacking history, shaping the future
               </h2>
             </div>
-            <div className='hidden md:flex flex-col'>
-              <h3 className='font-semibold text-xl'>6-8th October 2023</h3>
+            <div className='flex-col hidden md:flex'>
+              <h3 className='text-xl font-semibold'>6-8th October 2023</h3>
               <p className='text-xl'>Governance & Privacy Hackathon</p>
             </div>
-            <div className='flex flex-col md:flex-row items-center space-x-0 md:space-x-4 mt-4 w-5/6 space-y-4 md:space-y-0'>
-              <div
-                onClick={() => window.open('/ETHROME_SPONSOR_DECK.pdf', '_blank')}
-                className='w-full md:w-40 bg-[#8314C7] hover:bg-[#5b0e8b] transition-colors drop-shadow-[0_10px_8px_rgba(131,20,199,0.25)] rounded-full flex items-center justify-center text-white py-2 font-medium cursor-pointer'
-              >
-                <p>Sponsor deck</p>
-              </div>
+            <div className='flex flex-col items-center w-5/6 mt-4 space-x-0 space-y-4 md:flex-row md:space-x-4 md:space-y-0'>
+              <SponsorDeckLink className='w-full md:w-40 bg-[#8314C7] hover:bg-[#5b0e8b] transition-colors drop-shadow-[0_10px_8px_rgba(131,20,199,0.25)] rounded-full flex items-center justify-center text-white py-2 font-medium'/>
 
               <div
                 onClick={() => window.open('https://twitter.com/ethereumrome', '_blank')}
@@ -46,13 +41,13 @@ export default function Hero() {
           </div>
         </div>
 
-        <div className='flex md:hidden flex-col text-black z-10 text-center absolute bottom-12'>
-          <h3 className='font-semibold text-xl'>6-8th October 2023</h3>
+        <div className='absolute z-10 flex flex-col text-center text-black md:hidden bottom-12'>
+          <h3 className='text-xl font-semibold'>6-8th October 2023</h3>
           <p className='text-xl'>Governance & Privacy Hackathon</p>
         </div>
         <img
           src='splash_white_mobile.png'
-          className='block md:hidden absolute -bottom-56 w-full z-0'
+          className='absolute z-0 block w-full md:hidden -bottom-56'
         />
       </div>
     </div>
--- src/components/SponsorZone.tsx
+++ src/components/SponsorZone.tsx
@@ -1,22 +1,26 @@
+import { FC } from 'react'
+import { PropsWithClassName } from '../types'
+
+export const SponsorDeckLink: FC<PropsWithClassName> = ({ className }) => (
+  <a href="/ETHROME_SPONSOR_DECK.pdf" target="_blank" className={className}>
+    <p>Sponsor deck</p>
+  </a>
+)
+
 export default function SponsorZone() {
   return (
     <div
       className='min-h-screen md:h-auto bg-[#191818] relative pt-8 md:pt-32 pb-48 xl:py-48 pl-0 md:pl-48'
       id='contact'
     >
-      <div className='flex flex-col justify-center items-center md:items-start max-w-xl space-y-4 relative pb-24'>
+      <div className='relative flex flex-col items-center justify-center max-w-xl pb-24 space-y-4 md:items-start'>
         <img src='/sponsor_crown.svg' className='h-[160px] w-[160px]' />
-        <h1 className='font-bold text-white text-5xl md:text-7xl'>Sponsor Zone</h1>
+        <h1 className='text-5xl font-bold text-white md:text-7xl'>Sponsor Zone</h1>
         <p className='text-white max-w-xs md:max-w-[385px] text-center md:text-left'>
           Seize the chance to gain exceptional exposure, establish valuable connections, and
           demonstrate your unwavering support for the development of the blockchain space!
         </p>
-        <div
-          onClick={() => window.open('/ETHROME_SPONSOR_DECK.pdf', '_blank')}
-          className='w-40 hover:bg-[#5b0e8b] transition-colors bg-[#8314C7] drop-shadow-[0_10px_8px_rgba(131,20,199,0.25)] rounded-full flex items-center justify-center text-white py-2 font-medium cursor-pointer'
-        >
-          <p>Sponsor deck</p>
-        </div>
+        <SponsorDeckLink className='w-40 hover:bg-[#5b0e8b] transition-colors bg-[#8314C7] drop-shadow-[0_10px_8px_rgba(131,20,199,0.25)] rounded-full flex items-center justify-center text-white py-2 font-medium' />
         <a
           href='mailto:sponsor@ethrome.org'
           className='text-[#F8FCFC] opacity-60 underline text-md'
