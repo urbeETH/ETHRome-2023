b740d2f8a126784765dd389dc3076e220d8f646b
valerio <valerio.coltre@gmail.com>
2023-05-18 15:49:21 +0200

    new logos

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -2,21 +2,24 @@ import ImageLink from './ImageLink';
 export default function Partners() {
     return (
         <div className='py-16 md:py-24 bg-[#EFEBE6] flex flex-col items-center justify-center'>
-            <h4 className='uppercase opacity-60 text-black font-semibold'>Partners</h4>
-            <div className='py-16 grid grid-cols-2 md:grid-cols-4 gap-x-6 md:gap-x-32'>
-                <ImageLink imagePath="/partners/logo-ledger.png" url="https://www.ledger.com/" alt="logo-ledger"/>
-                <a href='https://web3privacy.info/' target='_blank' rel='noreferrer'><img
-                    src='/partners/logo-web3privacy.png' className='h-20 md:h-32'/></a>
-                <ImageLink imagePath="/partners/logo-openq.png" url="https://openq.dev/" alt="logo-openq"/>
-                <ImageLink imagePath="/partners/logo-rekt.png" url="https://rekt.news/" alt="logo-rekt"/>
-                <ImageLink imagePath="/partners/logo-ethdam.png" url="https://ethdam.com/" alt="logo-ethdam"/>
-                <ImageLink imagePath="/partners/logo-ethbrno.png" url="https://ethbrno.cz/" alt="logo-ethbrno"/>
-                <ImageLink imagePath="/partners/logo-bankless.png" url="https://www.bankless.community/" alt="logo-bankless"/>
-                <ImageLink imagePath="/partners/logo-lunardao.png" url="https://lunardao.net/" alt="logo-lunardao"/>
-                <ImageLink imagePath="/partners/logo-yomi.png" url="https://yomi.digital/#/" alt="logo-yomi"/>
-                <ImageLink imagePath="/partners/logo-spaghetteth.png" url="https://www.spaghett-eth.com/" alt="logo-spaghettet"/>
-                <ImageLink imagePath="/partners/logo-tag.png" url="https://talentgarden.org/" alt="logo-tag"/>
-                <ImageLink imagePath="/partners/logo-based_in_lisbon.png" url="https://www.basedinlisbon.xyz/" alt="logo-based-in-lisbon"/>
+            <h4 className='font-semibold text-black uppercase opacity-60'>Partners</h4>
+            <div className='grid grid-cols-2 py-16 md:grid-cols-4 gap-x-6 md:gap-x-32'>
+                <ImageLink imagePath="/partners/logo-aragon.png" url="https://aragonresearch.com/" alt="Logo Aragon Research"/>
+                <a href='https://web3privacy.info/' target='_blank' rel='noreferrer'>
+                    <img src='/partners/logo-web3privacy.png' className='h-20 md:h-32' alt="Logo Web3 Privacy"/>
+                </a>
+                <ImageLink imagePath="/partners/logo-openq.png" url="https://openq.dev/" alt="Logo OpenQ"/>
+                <ImageLink imagePath="/partners/logo-rekt.png" url="https://rekt.news/" alt="Logo Rekt"/>
+                <ImageLink imagePath="/partners/logo-ethdam.png" url="https://ethdam.com/" alt="Logo ETHdam"/>
+                <ImageLink imagePath="/partners/logo-ethbrno.png" url="https://ethbrno.cz/" alt="Logo ethBRNo"/>
+                <ImageLink imagePath="/partners/logo-bankless.png" url="https://www.bankless.community/" alt="Logo Bankless"/>
+                <ImageLink imagePath="/partners/logo-ledger.png" url="https://www.ledger.com/" alt="Logo Ledger"/>
+                <ImageLink imagePath="/partners/logo-lunardao.png" url="https://lunardao.net/" alt="Logo LunarDAO"/>
+                <ImageLink imagePath="/partners/logo-yomi.png" url="https://yomi.digital/#/" alt="Logo Yomi"/>
+                <ImageLink imagePath="/partners/logo-spaghetteth.png" url="https://www.spaghett-eth.com/" alt="Logo SpaghEtTH"/>
+                <ImageLink imagePath="/partners/logo-codemotion.png" url="https://www.codemotion.com/" alt="Logo Codemotion"/>
+                <ImageLink imagePath="/partners/logo-tag.png" url="https://talentgarden.org/" alt="Logo Talent Garden"/>
+                <ImageLink imagePath="/partners/logo-based_in_lisbon.png" url="https://www.basedinlisbon.xyz/" alt="Logo Based in Lisbon"/>
             </div>
         </div>
     )
