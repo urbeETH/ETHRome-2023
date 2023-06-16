911c637945228ee19375a9f3b28e23fdb472fe76
colthreepv <valerio.coltre@gmail.com>
2023-06-16 14:58:59 +0200

    privacy into encryption games

--- src/components/Hackathon.tsx
+++ src/components/Hackathon.tsx
@@ -9,10 +9,9 @@ export default function Hackathon() {
         <div className='flex flex-col items-center w-full space-y-8 md:flex-row md:items-start justify-evenly md:space-y-0'>
           <div className='flex flex-col items-center max-w-xs space-y-2'>
             <img src='/icon-spot-helmet-shield.svg' className='mb-2' />
-            <h2 className='text-3xl text-[#A81010] font-bold'>Game of ZKs</h2>
+            <h2 className='text-3xl text-[#A81010] font-bold'>Encryption Games</h2>
             <p className='text-center font-redhat'>
-              This track will challenge hackers to explore the full potential of ZKP technologies
-              and develop novel{' '}
+              This track will challenge hackers to explore the full potential of all those technologies and{' '}
               <span className='font-bold'>applications that prioritize privacy and security</span>.
             </p>
           </div>
