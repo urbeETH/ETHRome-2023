6cd4a9d2c3b7cc17482f9057c2823bd3a0767897
erminio-ricciardi <erminio.ricciardi@gmail.com>
2023-07-10 23:57:48 +0200

    Aesthetics Update
    - button Intro resize
    - ETHRome Article text resize
    - default scroll bar

--- src/Article/ETHRomeArticle.tsx
+++ src/Article/ETHRomeArticle.tsx
@@ -21,9 +21,9 @@ export default function Article() {
                 by builders, for builders
               </h1>
             </div>
-            <div className='flex space-x-0 md:space-x-24 justify-between'>
+            <div className='flex md:w-3/4 space-x-0 md:space-x-24 justify-between text-justify'>
               <div className='flex flex-col'>
-                <p className='mt-2 ml-2 text-lg font-light text-white md:text-xl font-redhat'>
+                <p className='text-lg font-light text-white md:text-xl font-redhat'>
                   This isn&apos; t a battle of antiquated versus modern, but rather a timeless
                   struggle woven into the fabric of human history: the clash between centralisation
                   and decentralization. It&apos;s the conflict between societal structures where
@@ -36,7 +36,7 @@ export default function Article() {
                 <div className='w-full flex justify-center mt-8 mb-8'>
                   <img src='/article_1_img.jpg' alt='' />
                 </div>
-                <p className='mt-2 ml-2 text-lg font-light text-white md:text-xl font-redhat'>
+                <p className='text-lg font-light text-white md:text-xl font-redhat'>
                   Time has witnessed colossal shifts since then, and Barlow&apos;s guiding
                   principles, once envisaged as the cornerstone of Web 2.0, have been overshadowed
                   by an unprecedented wave of centralisation. Technological behemoths have arisen,
@@ -45,7 +45,7 @@ export default function Article() {
                   the right to privacy protected, or how any wealth generated from this data would
                   be redistributed back to the society and the individuals who provided it.
                 </p>
-                <p className='mt-2 ml-2 text-lg font-light text-white md:text-xl font-redhat'>
+                <p className='text-lg font-light text-white md:text-xl font-redhat'>
                   This is the chronicle of our era which we are already well-acquainted with.
                   Digital behemoths have become unquenchably greedy, creating walled environments in
                   which we can satisfy many needs, but at the cost of surrendering control,
@@ -70,9 +70,9 @@ export default function Article() {
             <div className='w-full flex justify-center mt-8 mb-8'>
               <img className='w-4/5 md:w-5/12' src='/Urbe+logoOK 1.svg' alt='' />
             </div>
-            <div className='flex space-x-0 md:space-x-24 justify-between'>
+            <div className='flex md:w-3/4 space-x-0 md:space-x-24 justify-between text-justify'>
               <div className='flex flex-col'>
-                <p className='mt-2 ml-2 text-lg font-light text-white md:text-xl font-redhat'>
+                <p className='text-lg font-light text-white md:text-xl font-redhat'>
                   In this whirlwind of technologies, ideas, and initiatives, in May 2022 a group of
                   devs based in Rome and with a particular interest in Web3 and decentralization,
                   launched <span className='underline'>urbe.eth</span>. The community was initiated
@@ -83,7 +83,7 @@ export default function Article() {
                 <div className='w-full flex justify-center mt-8 mb-8'>
                   <img src='/UrbeTalkArticle.jpg' alt='' />
                 </div>
-                <p className='mt-2 ml-2 text-lg font-light text-white md:text-xl font-redhat'>
+                <p className='text-lg font-light text-white md:text-xl font-redhat'>
                   Summoning periodically at co-workings and pubs, Urbe&apos;s numbers began to
                   increase, reaching over 500 people on Discord, +2k on Twitter and 30 to 50
                   attending regular IRL meetups. At this point, the next progression was inevitable.
@@ -91,14 +91,14 @@ export default function Article() {
                 <div className='w-full flex justify-center mt-8 mb-8'>
                   <img src='/UrbeDelegationArticle.jpg' alt='' />
                 </div>
-                <p className='mt-2 ml-2 text-lg font-light text-white md:text-xl font-redhat'>
+                <p className='text-lg font-light text-white md:text-xl font-redhat text-center'>
                   Most of Urbe&apos;s lead Devs are regular attendees of large conferences and
                   hackathons worldwide, hence the idea: why not organize one in Rome?
                 </p>
                 <div className='w-full flex justify-center mt-8 mb-8'>
                   <img className='w-4/6 md:w-1/6' src='/ethrome_logo_article.svg' alt='' />
                 </div>
-                <p className='mt-2 ml-2 text-lg font-light text-white md:text-xl font-redhat'>
+                <p className='text-lg font-light text-white md:text-xl font-redhat'>
                   With its unique allure, Rome provides an idyllic backdrop for any event. Its
                   architectural marvels and sumptuous cuisine are the perfect framework for
                   delightful interludes. The city&apos;s millennia-old history weaves a vital
@@ -116,7 +116,7 @@ export default function Article() {
                 <div className='w-full flex justify-center mt-8 mb-8'>
                   <img src='/slideArticle.png' alt='' />
                 </div>
-                <p className='mt-2 ml-2 text-lg font-light text-white md:text-xl font-redhat md:mb-8'>
+                <p className='text-lg font-light text-white md:text-xl font-redhat md:mb-8'>
                   The parts of the crypto space that are honorable are those that actually stay true
                   to its core principles of decentralization, privacy, openness, and transparency.
                   We intend to pursue and uphold these values by summoning key people from all over
