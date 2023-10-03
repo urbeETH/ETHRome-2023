f76e75b056f9c699a0ed49ebefccd9191b40e02e
erminio-ricciardi <erminio.ricciardi@gmail.com>
2023-10-04 00:25:24 +0200

    CryptoEvents PartnersCollaborators Update

--- src/components/PartnersCollaborators.tsx
+++ src/components/PartnersCollaborators.tsx
@@ -5,37 +5,38 @@ export default function PartnersCollaborats() {
       <div className='flex flex-col justify-center items-start md:flex-row w-3xl gap-6'>
         <div className='flex flex-col items-center gap-2'>
           <h4 className='font-semibold text-rome-red uppercase'>MEDIA PARTNERS</h4>
-          <div className='grid grid-cols-2 gap-4'>
-            <div className='flex justify-center items-center shrink-0 w-[180px] h-[108px] opacity-60'>
+          <div className='partnersCollab_grid gap-4'>
+            <div className='partnersCollab_item flex justify-center items-center shrink-0 w-[180px] h-[108px] opacity-60'>
               <Image
                 imagePath='/partnersCollaborators/logo-rekt.svg'
                 url='https://rekt.news'
                 alt='Logo Rekt'
-
               />
             </div>
-            <div className='flex justify-center items-center shrink-0 w-[180px] h-[108px] opacity-60'>
+            <div className='partnersCollab_item flex justify-center items-center shrink-0 w-[180px] h-[108px] opacity-60'>
               <Image
                 imagePath='/partnersCollaborators/logo-banklessDao.png'
                 url='https://www.bankless.community/'
                 alt='Logo BanklessDAO'
-
               />
             </div>
-            <div className='flex justify-center items-center shrink-0 w-[180px] h-[108px] opacity-60'>
-              <Image
-                imagePath='/partnersCollaborators/logo-forweb3.svg'
-                url='https://forwebthree.com/'
-                alt='Logo for web3'
-
-              />
+            <div className='partnersCollab_item flex justify-center items-center shrink-0 w-[180px] h-[108px] opacity-60'>
+              <a href="https://forwebthree.com/" className='flex justify-center items-center'>
+                <img className='md:w-[70%]' src="/partnersCollaborators/logo-forweb3.svg" alt="Logo For Web3" />
+              </a>
             </div>
-            <div className='flex justify-center items-center shrink-0 w-[180px] h-[108px] opacity-60'>
+            <div className='partnersCollab_item flex justify-center items-center shrink-0 w-[180px] h-[108px] opacity-60'>
               <Image
                 imagePath='/partnersCollaborators/logo-blockTides.png'
                 url='https://blocktides.com/'
                 alt='Logo Block Tides'
-
+              />
+            </div>
+            <div className='partnersCollab_item flex justify-center items-center shrink-0 w-[180px] h-[108px] opacity-60'>
+              <Image
+                imagePath='/partnersCollaborators/cryptoEvents.png'
+                url='https://cryptoevents.xyz/'
+                alt='Logo Crypto Events'
               />
             </div>
           </div>
@@ -54,9 +55,7 @@ export default function PartnersCollaborats() {
             </div>
           </div>
           <div className='flex flex-col items-center gap-2'>
-            <h4 className='font-semibold text-rome-red uppercase'>
-              HACKATHON COLLABORATOR
-            </h4>
+            <h4 className='font-semibold text-rome-red uppercase'>HACKATHON COLLABORATOR</h4>
             <div className='flex w-[220px] h-[132px] justify-center items-center'>
               <Image
                 imagePath='/partnersCollaborators/logo-layerX.svg'
@@ -64,7 +63,6 @@ export default function PartnersCollaborats() {
                 alt='Logo LayerX'
                 className='h-16 md:h-24 opacity-60'
               />
-
             </div>
           </div>
         </div>
